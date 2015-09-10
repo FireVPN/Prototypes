635b93a3e3792163d47418dc00485027903102e3
alexandermark97 <alexander.mark@edu.htl.rennweg.at>
2015-09-10 17:10:03 +0200

    refrectoring und code analyse
    Signed-off-by: alexandermark97 <alexander.mark@edu.htl.rennweg.at>

--- Hole Punching/tcp_hole_puncher.py
+++ Hole Punching/tcp_hole_puncher.py
@@ -1,31 +1,36 @@
 __author__ = 'Alexander Mark, FireVPN'
 # uses Python 3.4 Interpreter
+# sys.argv[1] == IP Addresse (destination)
+# sys.argv[3] == Source Port
+# sys.argv[2] == Destination Port
 
 import sys
 import socket
 import _thread as thread
 
 def client():
-    c = socket.socket()
+    client_socket = socket.socket()
 
-    c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-    c.bind('', int(sys.argv[3]))
-    while(c.connect_ex((sys.argv[1], int(sys.argv[2])))):
+    #c.bind((socket.gethostbyname(socket.gethostname()), int(sys.argv[3])))
+    client_socket.bind('', int(sys.argv[3]))
+    while(client_socket.connect_ex((sys.argv[1], int(sys.argv[2])))):
         pass
     print("connected!")
     thread.interrupt_main()
 
 def server():
-    c = socket.socket()
+    srv_socket = socket.socket()
 
-    c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-    c.bind('', int(sys.argv[3]))
-    c.listen(5)
-    c.accept()
+    #c.bind((socket.gethostbyname(socket.gethostname()), int(sys.argv[3])))
+    srv_socket.bind('', int(sys.argv[3]))
+    srv_socket.listen(5)
+    srv_socket.accept()
     print("connected!")
     thread.interrupt_main()
 
