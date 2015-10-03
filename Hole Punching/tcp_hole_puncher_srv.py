1950a07888e70e2715a588e9c0b94fca756366c4
alexandermark97 <alexander.mark@edu.htl.rennweg.at>
2015-10-03 10:21:16 +0200

    Server ist multithreaded, syncronisierung des sets fehlt noch
    Signed-off-by: alexandermark97 <alexander.mark@edu.htl.rennweg.at>

--- Hole Punching/tcp_hole_puncher_srv.py
+++ Hole Punching/tcp_hole_puncher_srv.py
@@ -3,6 +3,7 @@ __author__ = 'Alexander Mark, FireVPN'
 import socket
 import sys
 import pickle
+import _thread as thread
 
 port = 9998
 ip= "10.0.0.8"#(socket.gethostbyname(socket.gethostname()))
@@ -19,37 +20,39 @@ srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #Port wieder ve
 srv_socket.bind((ip, port)) #IP + Port binden
 srv_socket.listen(5) # auf Verbindungsanfragen lauschen
 
-def receive():
-    clientsocket,addr = srv_socket.accept() #verbindung annehmen
-    data, addr = srv_socket.recvfrom(1024) #daten und adresse in variablen speichern
-    host = addr[0]
-    port = addr[1]
-    utf_data = data.decode('utf-8') #charset fuer daten festlegen
+def receive(clientsocket,addr):
+    while (True):
+        data, addr = srv_socket.recvfrom(1024) #daten und adresse in variablen speichern
+        host = addr[0]
+        port = addr[1]
+        utf_data = data.decode('utf-8') #charset fuer daten festlegen
 
-    if utf_data is 'L': #wenn L empfangen wurde, addresse in set eintragen
-        logins.add((host, port))
-        print (logins)
+        if utf_data is 'L': #wenn L empfangen wurde, addresse in set eintragen
+            logins.add((host, port))
+            print (logins)
 
-    if utf_data is 'R': # wenn R empfangen wurde, addressen vom set an client senden
-        try:
-            clientsocket.send(pickle.dumps(logins))
-        except:
-            print ("Could not send. R")
-        print ("sent reload. R")
+        if utf_data is 'R': # wenn R empfangen wurde, addressen vom set an client senden
+            try:
+                clientsocket.send(pickle.dumps(logins))
+            except:
+                print ("Could not send. R")
+            print ("sent reload. R")
 
-    if utf_data is 'LR': # wenn LR empfangen wurde, addressen in set speichern, und danach zuruecksenden an Client
-        logins.add((host, port))
-        print (logins)
-        try:
-            clientsocket.send(pickle.dumps(logins))
-        except:
-            print ("Could not send. LR")
-        print ("sent reload. LR")
-    clientsocket.close()
+        if utf_data is 'LR': # wenn LR empfangen wurde, addressen in set speichern, und danach zuruecksenden an Client
+            logins.add((host, port))
+            print (logins)
+            try:
+                clientsocket.send(pickle.dumps(logins))
+            except:
+                print ("Could not send. LR")
+            print ("sent reload. LR")
+
+        if utf_data is 'CLOSE': # wenn CLOSE empfangen wurde, Verbindung trennen
+            clientsocket.close()
 
 def main():
     while True:
-        receive()
+        thread.start_new_thread(receive(srv_socket.accept()))# bei annahme der Verbindung recive() aufrufen
 
 if __name__ == '__main__':
     main()
