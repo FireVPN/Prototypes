204d25e1a17b9709f53be40df830e7be4268c7c1
alexandermark97 <alexander.mark@edu.htl.rennweg.at>
2015-10-11 21:33:33 +0200

    multithreading buggs noch nicht behoben, refractort
    Signed-off-by: alexandermark97 <alexander.mark@edu.htl.rennweg.at>

--- Hole Punching/Python3_TCP/gui.py
+++ Hole Punching/Python3_TCP/gui.py
@@ -1,4 +1,4 @@
-__author__ = 'alexander'
+__author__ = 'Alexander Mark, FireVPN'
 
 import sys
 import socket
@@ -20,39 +20,60 @@ class Client(QtGui.QWidget):
 
         self.initUI()
 
+    #GUI
     def initUI(self):
 
+        #connect to server button erstellen
         qbtn_connect_srv = QtGui.QPushButton('Connect to Server', self)
-        QtGui.QWidget.connect(qbtn_connect_srv, QtCore.SIGNAL("clicked()"), self.foo)
+        QtGui.QWidget.connect(qbtn_connect_srv, QtCore.SIGNAL("clicked()"), self.init_srv_connct)
         #qbtn_connect_srv.clicked.connect(thread.start_new_thread(Controller.connect_to_server(self)))
         #QtGui.QWidget.connect(qbtn_connect_srv, QtCore.SIGNAL("clicked()"), thread.Thread(self.connect_to_server()).start())
         qbtn_connect_srv.resize(qbtn_connect_srv.sizeHint())
 
+        #combobox erstellen
         address_box=QtGui.QComboBox(self)
         address_box.addItem("IP : Port")
         address_box.resize(address_box.sizeHint())
         address_box.move(0, 30)
         self.address_box=address_box
 
+        #connect to client button erstellen
         qbtn_connect_client = QtGui.QPushButton('Connect to Client', self)
-        QtGui.QWidget.connect(qbtn_connect_client, QtCore.SIGNAL("clicked()"), self.foo)
+        QtGui.QWidget.connect(qbtn_connect_client, QtCore.SIGNAL("clicked()"), self.init_client_connect)
         #qbtn_connect_client.clicked.connect(Controller.foo(self))
         qbtn_connect_client.resize(qbtn_connect_client.sizeHint())
         qbtn_connect_client.move(100, 30)
 
+        #quit button erstellen
         qbtn_quit = QtGui.QPushButton('Quit', self)
         qbtn_quit.clicked.connect(QtCore.QCoreApplication.instance().quit)
         qbtn_quit.resize(qbtn_quit.sizeHint())
         qbtn_quit.move(0, 60)
 
+        #launch
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('TCP Hole-Puncher')
         self.show()
         #qbtn_connect_srv.clicked.connect(self.connect_to_server())
 
-    def foo(self):
+    #verbindung zu server initieren
+    def init_srv_connct(self):
         t1=thread.Thread(target=self.connect_to_server())
         t1.start()
+
+    #connect to client initieren
+    def init_client_connect(self):
+        ip=""
+        port=0
+
+        entry=self.address_box.activated[str](self)
+        ip=entry.split(":")[0]
+        port=int(entry.spilt(":")[1])
+
+        t1=thread.Thread(target=self.connect_to_client(ip, port))
+        t2=thread.Thread(target=self.listen())
+        t1.start()
+        t2.start()
 
 
     #sendet syn Packete aus waehrend des hole punching prozesses
@@ -128,6 +149,7 @@ class Client(QtGui.QWidget):
 
         #thread.join()
 
+    #combobox aktuallisieren von den ergebnissen des servers
     def update_combo_box(self):
         for i in self.logins:
             self.address_box.addItem(i[0]+":"+str(i[1]))
