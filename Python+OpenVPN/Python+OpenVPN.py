ec81748007e65a6fde1de92e0cdb8bf6a1821a35
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-10-23 23:59:09 +0200

    Python+OpenVPN grundsätzlich programmiert

--- Python+OpenVPN/Python+OpenVPN.py
+++ Python+OpenVPN/Python+OpenVPN.py
@@ -1,1 +1,23 @@
-__author__ = 'User'
+__author__ = 'Elias Eckenfellner'
+import time, sys
+IP="192.168.0.1"
+PORT="90"
+
+class Client():
+    def __init__(self, ip):
+        self.send()
+    def send(self):
+        ip = self.variable.get()
+        port = None
+        try:
+            self.sock.sendto(self.text.get().encode('utf-8'), (IP, PORT))
+            time.sleep(1)
+        except:
+            print ("Open UDP Socket")
+            sys.exit(1)
+
+def main():
+    client = Client()
+
+if __name__ == '__main__':
+    main()
