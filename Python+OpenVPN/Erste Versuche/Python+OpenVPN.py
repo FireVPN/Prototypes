e0011cea3ea34cf9cc4ab43f064bda8b2eac4e81
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-01 16:40:53 +0100

    Python+OpenVPN umstrukturiert

---
+++ Python+OpenVPN/Erste Versuche/Python+OpenVPN.py
@@ -0,0 +1,28 @@
+__author__ = 'Elias Eckenfellner'
+import time, sys, socket
+IP="192.168.0.9"
+PORT=6222
+
+class Client():
+    def __init__(self):
+        self.send()
+    def send(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except:
+            print ("UDP-Socket konnte nicht geöffnet werden!")
+            sys.exit(1)
+        try:
+            sock.sendto("Hier wird über den UDP-Socket gesendet!".encode('utf-8'), (IP, PORT))
+            time.sleep(1)
+        except:
+            print ("Über den UDP-Socket konnte nichts gesendet werden!")
+            sys.exit(1)
+        print ("Socket steht und es wurde schon darüber übertragen")
+        time.sleep(20)
+
+def main():
+    client = Client()
+
+if __name__ == '__main__':
+    main()
