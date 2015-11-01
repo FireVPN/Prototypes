51974b1a485a65d356d06f342820e41a220c4ea9
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-01 18:53:44 +0100

    Proxy.py fast fertig

--- Python+OpenVPN/Proxy/proxy.py
+++ Python+OpenVPN/Proxy/proxy.py
@@ -1,9 +1,14 @@
 import socket, sys, threading
 
+#Gegenüber
 PEER=("10.0.0.2",6222)
 LOCALVPN=""
+#Wenn hier der Server läuft, dann True
+SERVER=True
 
+#Externe IP
 EXT=("10.0.0.1", 6222)
+#Default - Localhost
 INT=("127.0.0.1", 6221)
 
 try:
@@ -21,15 +26,19 @@ except:
     sys.exit(1)
 
 def int_to_ext():
+    #32768
     while True:
-        data, addr = socket_int.recvfrom(32768)
+        data, addr = socket_int.recvfrom(1024)
         LOCALVPN=addr
         socket_ext.sendto(data, PEER)
 
 def ext_to_int():
     while True:
-        data, addr = socket_ext.recvfrom(32768)
-        socket_int.sendto(data, LOCALVPN)
+        data, addr = socket_ext.recvfrom(1024)
+        if SERVER==True:
+            socket_int.sendto(data, INT)
+        else:
+            socket_int.sendto(data, LOCALVPN)
 
 
 
