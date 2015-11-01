675136e479557ab1ce86539f86e9c93328b980c6
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-01 18:13:32 +0100

    Konzept geändert(statt 'forward' zwei Funktionen

--- Python+OpenVPN/Proxy/proxy.py
+++ Python+OpenVPN/Proxy/proxy.py
@@ -1,14 +1,39 @@
-import sys, threading
+import socket, sys, threading
 
-EXT=("10.0.0.2", 6222)
-LOCAL=("127.0.0.1", 6221)
+PEER=("10.0.0.2",6222)
+LOCALVPN=""
 
-def forward(listen):
-    import socket
-    try:
-        socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        socket.bind(listen)
-    except:
-        print ("Could not set up socket.")
-        sys.exit(1)
+EXT=("10.0.0.1", 6222)
+INT=("127.0.0.1", 6221)
 
+try:
+    socket_int = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_int.bind(INT)
+except:
+    print ("Could not set up INT socket.")
+    sys.exit(1)
+
+try:
+    socket_ext = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_ext.bind(EXT)
+except:
+    print ("Could not set up EXT socket.")
+    sys.exit(1)
+
+def int_to_ext():
+    while True:
+        data, addr = socket_int.recvfrom(32768)
+        LOCALVPN=addr
+        socket_ext.sendto(data, PEER)
+
+def ext_to_int():
+    while True:
+        data, addr = socket_ext.recvfrom(32768)
+        socket_int.sendto(data, LOCALVPN)
+
+
+
+t_int_to_ext = threading.Thread(target=int_to_ext)
+t_ext_to_int = threading.Thread(target=ext_to_int)
+t_int_to_ext.start()
+t_ext_to_int.start()
