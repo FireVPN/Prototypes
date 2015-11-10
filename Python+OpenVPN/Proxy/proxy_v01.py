2e5db9a797dceac136692bb2ec77e37f5d4bb803
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-10 23:02:57 +0100

    proxy_v02_asyncio.py erstelllt, Author geändert

--- Python+OpenVPN/Proxy/proxy_v01.py
+++ Python+OpenVPN/Proxy/proxy_v01.py
@@ -1,45 +1,27 @@
+__author__ = 'Elias Eckenfellner'
+
 import socket, sys, threading
 
 SERVER=True
 PEER=("10.0.0.2", 6222)
-LOCALVPN=("localhost", 6220)
+#Wenn Server
+LOCALVPN=("127.0.0.1", 6220)
 
 EXT=("10.0.0.1", 6222)
-INT=("localhost", 6221)
+INT=("127.0.0.1", 6221)
 
 #Immer 0
 LOCALVPNPORT=0
 
-#try:
-socket_int_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_int_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_int_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_int_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#socket_int.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-socket_int_rec.bind(INT)
-socket_int_send.bind((INT[0], (INT[1]-1)))
-#except:
-    #print ("Could not set up INT socket.")
-    #sys.exit(1)
 
-#try:
+socket_int_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#socket_int_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_int_rec.bind(INT)
+
+
 socket_ext_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_ext_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_ext_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_ext_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#socket_ext.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 socket_ext_rec.bind(EXT)
-socket_ext_send.bind(EXT)
-
-
-
-#socket_int_rec2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#socket_int_rec2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#socket_int_rec2.bind(INT)
-
-#except:
-    #print ("Could not set up EXT socket.")
-    #sys.exit(1)
 
 def int_to_ext():
     global LOCALVPNPORT
@@ -63,11 +45,11 @@ def ext_to_int():
     while True:
         data, addr = socket_ext_rec.recvfrom(4096)
         if(not SERVER):
-            socket_int_send.sendto(data, ("localhost", LOCALVPNPORT))
+            socket_int_send.sendto(data, (INT[0], LOCALVPNPORT))
         if(SERVER):
-            socket_int_send.sendto(data, LOCALVPN)
+            #socket_int_send.sendto(data, LOCALVPN)
+            socket_int_rec.sendto(data, LOCALVPN)
         print("ext to int")
-
 
 
 t_int_to_ext = threading.Thread(target=int_to_ext)
