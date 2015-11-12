36682d621ee353cce8bdee6cac696e10794c2aec
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-12 01:05:26 +0100

    Server+Client zusammengeführt & Kommentiert

--- Python+OpenVPN/Proxy/proxy_v02_asyncio.py
+++ Python+OpenVPN/Proxy/proxy_v02_asyncio.py
@@ -1,82 +1,105 @@
 __author__ = 'Elias Eckenfellner'
-HOST="127.0.0.1"
-PORT=56
 import argparse
 import sys
 import asyncio
 import signal
+import socket
 
-SERVER=True
-#Gegenüber von FireVPN
-PEER=("10.0.0.2", 6222)
-#Wenn Server
+#Ob Proxy auf der Server- oder Client-Seite läuft
+SERVER=False
+#Gegenüber
+PEER_IP="10.0.0.2"
+#Eigene externe IP
+EXT_IP="10.0.0.1"
+
+##########
+#Statisch#
+##########
+#Externer Port beider Seiten
+PORTEXT=6222
+#Interne IP und Port(Localhost)
+INT=("127.0.0.1", 6221)
+#Interne IP und Port des VPN-Servers/Clients(Localhost)  --- Port bei Client unbekannt
 LOCALVPN=("127.0.0.1", 6220)
 
-EXT=("10.0.0.1", 6222)
-INT=("127.0.0.1", 6221)
+PEER=(PEER_IP, PORTEXT)
+EXT=(EXT_IP, PORTEXT)
 
-#Immer 0
+#Socket um bei "int to ext" ext zu senden
+ext=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ext.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ext.bind(EXT)
+
+#Socket um bei "ext to int" int zu senden
+int=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+int.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+int.bind(INT)
+
+#Vom Programm benötigte globale Variable
 LOCALVPNPORT=0
 
-class Serverrelay:
+#Innen empfangen und nach aussen senden
+class INTERN:
+  def connection_made(self, transport):
+      self.transport = transport
 
-    def connection_made(self, transport):
-        self.transport = transport
+  def datagram_received(self, data, addr):
+      global ext
+      global SERVER
+      global LOCALVPNPORT
 
-    def datagram_received(self, data, addr):
-        print("datagram recieved")
-        #Am Outside-Sockel senden
-        #self.transport.sendto(data, addr)
+      print("int to ext")
+      if(SERVER is False and LOCALVPNPORT is not 0):
+          LOCALVPNPORT=addr[1]
+       #Am Outside-Sockel senden
+      ext.sendto(data, PEER)
 
-    def error_received(self, exc):
-        print('Error received:', exc)
+  def error_received(self, exc):
+      print('Error received:', exc)
 
-    def connection_lost(self, exc):
-        print('stop', exc)
-
-
-class ClientRelay:
-
-    def connection_made(self, transport):
-        self.transport = transport
-        print('sending "{}"'.format(self.message))
-        self.transport.sendto(self.message.encode())
-        print('waiting to receive')
-
-    def datagram_received(self, data, addr):
-        print('received "{}"'.format(data.decode()))
-        self.transport.close()
-
-    def error_received(self, exc):
-        print('Error received:', exc)
-
-    def connection_lost(self, exc):
-        print('closing transport', exc)
-        loop = asyncio.get_event_loop()
-        loop.stop()
+  def connection_lost(self, exc):
+      print('stop', exc)
 
 
-def start_server(loop, addr):
-    t = asyncio.Task(loop.create_datagram_endpoint(
-        Serverrelay, local_addr=addr))
-    transport, server = loop.run_until_complete(t)
-    return transport
+#Aussen empfangen und innen senden
+class EXTERN:
+  def connection_made(self, transport):
+      self.transport = transport
+
+  def datagram_received(self, data, addr):
+      global int
+      global SERVER
+      global LOCALVPNPORT
+      print("ext to int")
+      #Am Inside-Sockel senden
+      if(SERVER is False):
+          int.sendto(data, ("127.0.0.1", LOCALVPNPORT))
+      else:
+          int.sendto(data, LOCALVPN)
+
+  def error_received(self, exc):
+      print('Error received:', exc)
+
+  def connection_lost(self, exc):
+      print('closing transport', exc)
 
 
-def start_client(loop, addr):
-    t = asyncio.Task(loop.create_datagram_endpoint(
-        ClientRelay, remote_addr=addr))
-    loop.run_until_complete(t)
+def start_intern(loop, addr):
+  t = asyncio.Task(loop.create_datagram_endpoint(
+      INTERN, local_addr=addr))
+  transport, server = loop.run_until_complete(t)
+
+
+def start_extern(loop, addr):
+  t = asyncio.Task(loop.create_datagram_endpoint(
+      EXTERN, remote_addr=addr))
+  loop.run_until_complete(t)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    if(SERVER):
-        server = start_server(loop, INT)
-    else:
-        start_client(loop, (HOST, PORT))
-    try:
-        loop.run_forever()
-    finally:
-        if(SERVER):
-            server.close()
-        loop.close()
+  loop = asyncio.get_event_loop()
+  intern_obj = start_intern(loop, INT)
+  extern_obj = start_extern(loop, EXT)
+  loop.run_forever()
+  intern_obj.close()
+  extern_obj.close()
+  loop.close()
