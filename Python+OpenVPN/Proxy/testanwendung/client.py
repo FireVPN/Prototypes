d660f02c1567234dc537aa071ae29755d2e2a1f3
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-19 20:34:40 +0100

    Client.py hinzugefügt

---
+++ Python+OpenVPN/Proxy/testanwendung/client.py
@@ -0,0 +1,13 @@
+__author__ = 'Elias Eckenfellner'
+import socket
+IP="localhost"
+#Vom OpenVPN-Client randomized
+PORT=6532
+
+PROXY=("localhost", 6221)
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP, PORT))
+print("Socket auf", IP,":",PORT, "gebunden")
+sock.sendto("Hello from Client".encode("UTF-8"), PROXY)
+print("An", PROXY, "gesendet")
