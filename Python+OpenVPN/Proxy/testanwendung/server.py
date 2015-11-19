3f57de5b55bf55bc62b1bb1c9e7428159c04367d
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-19 20:29:43 +0100

    Debug-Infos hinzugefügt

--- Python+OpenVPN/Proxy/testanwendung/server.py
+++ Python+OpenVPN/Proxy/testanwendung/server.py
@@ -5,6 +5,9 @@ PORT=6220
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, PORT))
+print("Server lauscht auf", IP,":",PORT)
 while True:
     data, addr = sock.recvfrom(2048)
+    print("von", addr[0],":",addr[1], "empfangen")
     sock.sendto("Hello from Server".encode("UTF-8"), addr)
+    print("an", addr[0],":",addr[1], "gesendet")
