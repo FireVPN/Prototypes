b4fcebdf73308d8414cd5b49b65a4ac72c0ad3fa
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-11-25 23:37:31 +0100

    verbesserungen in recv

--- Python+OpenVPN/Proxy/proxy4.py
+++ Python+OpenVPN/Proxy/proxy4.py
@@ -1,11 +1,10 @@
 __author__ = 'Elias Eckenfellner'
 import threading, socket
 
-
 class Proxy:
 
     def __init__(self, ip, port):
-        SERVER=False
+        self.SERVER=False
         LOCALVPNPORT=0
         self.sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((ip, port))
@@ -13,22 +12,23 @@ class Proxy:
 
     def recv(self):
         print("Recieving")
-        while True:
-            data, addr = self.sock.recvfrom(4096)
-            if not self.SERVER:
-                LOCALVPNPORT=addr[1]
-            #Kill den anderen
+        data, addr = self.sock.recvfrom(4096)
+        if not self.SERVER:
+            LOCALVPNPORT=addr[1]
+            #Kill den anderen recv[intern/extern]
         print("int to ext")
 
     def send(self):
         print("sending")
-        
+
 
 def main():
-    intern=Proxy("127.0.0.1", 6220)
-    extern=Proxy("10.0.0.1", 6221)
-    rec1=threading.Thread(target=intern.recv).start()
-    rec2=threading.Thread(target=extern.recv).start()
+    global externrecv
+    global internrecv
+    intern=Proxy("127.0.0.1", 6220, externrecv)
+    extern=Proxy("10.0.0.1", 6221, internrecv)
+    recvintern=threading.Thread(target=intern.recv).start()
+    recvextern=threading.Thread(target=extern.recv).start()
 
 if __name__ == '__main__':
     main()
