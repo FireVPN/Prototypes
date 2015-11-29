93cbf084315d262bae9410059f7bfb0e692ecf7f
alexandermark97 <alexander.mark@edu.htl.rennweg.at>
2015-11-29 11:41:41 +0100

    Verbindungsaufbau möglich

--- Hole Punching/Python3_TCP/new_Client.py
+++ Hole Punching/Python3_TCP/new_Client.py
@@ -1,3 +1,39 @@
 __author__ = 'Alexander Mark, FireVPN'
 
+import socket
+
+class Client():
+
+    def __init__(self):
+        print ("starting programm...")
+        self.ip=input("IP des Hosts: ")
+        self.port=4444
+        print("IP + Port from Host saved")
+
+
+    def connect_to_server(self):
+        srv_ip=input("IP des Servers: ")
+        srv_port=int(input("Port des Servers: "))
+        print ("Got Server Adress")
+
+        to_srv_sock = socket.socket()
+
+        to_srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Addresse wieder verwenden
+        to_srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1) #Port wieder verwenden
+
+        to_srv_sock.bind((self.ip, self.port)) #IP + Port "zusammenfuehren"
+        print("Set up Socket (for Server Communication)")
+
+        while(to_srv_sock.connect_ex((srv_ip, srv_port))): #SYN packets flooding
+            pass
+        print("connected to Server!")
+
+
+def main():
+    client=Client()
+    client.connect_to_server()
+
+
+if __name__ == '__main__':
+    main()
 
--- Hole Punching/Python3_TCP/new_Server.py
+++ Hole Punching/Python3_TCP/new_Server.py
@@ -1,3 +1,40 @@
 __author__ = 'Alexander Mark, FireVPN'
 
+import sys
+import socket
+import time
 
+""""
+Parameter nach dem File (zB.: python3 server.py 10.0.0.1 8888)
+sys.argv[1] private IP des Hosts (zB.: 10.0.0.1)
+sys.argv[2] Port des Hosts (zB.: 8888)
+"""""
+
+class Server():
+
+    def __init__(self):
+        self.sock = socket.socket()
+        self.sock.bind((sys.argv[1], int(sys.argv[2])))
+        print ("set up port on ", sys.argv[1], sys.argv[2])
+        self.sock.listen(5)
+        print ("Socket set to listen")
+
+    def accept_connection(self):
+        clientsocket =self.sock.accept()
+        print ("Got new Connection")
+        return True, clientsocket
+
+
+def main():
+    srv=Server()
+    while (True):
+        connection, clientsocket=srv.accept_connection()
+        if (connection):
+            #neuer Thread
+            print ("lala")
+        else:
+            time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
