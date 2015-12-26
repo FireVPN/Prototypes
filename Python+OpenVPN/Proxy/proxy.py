cf6a6c32974dfef36c18a76171f59d331228fbda
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-12-26 13:57:47 +0100

    refactoring

--- Python+OpenVPN/Proxy/proxy.py
+++ Python+OpenVPN/Proxy/proxy.py
@@ -8,80 +8,67 @@ EXT=("10.0.0.1", 6222)
 INT=("localhost", 6221)
 
 #Immer 0
-LOCALVPNPORT=0
+localvpnport=0
 
-#try:
-socket_int_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_int_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_int_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_int_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#socket_int.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-socket_int_rec.bind(INT)
-socket_int_send.bind(INT)
-#except:
-    #print ("Could not set up INT socket.")
-    #sys.exit(1)
+try:
+    socket_int_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_int_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_int_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_int_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_int_rec.bind(INT)
+    socket_int_send.bind(INT)
+    #Performance
+    socket_int_rec.settimeout(1)
+except:
+    print ("Interner Socket konnte nicht initialisiert werden!")
+    sys.exit(1)
 
-#try:
-socket_ext_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_ext_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_ext_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_ext_rec.setblocking(False)
-socket_ext_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-socket_ext_send.setblocking(False)
-#socket_ext.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-socket_ext_rec.bind(EXT)
-socket_ext_send.bind(EXT)
+try:
+    socket_ext_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_ext_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_ext_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_ext_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_ext_rec.bind(EXT)
+    socket_ext_send.bind(EXT)
+    #Performance
+    socket_ext_rec.settimeout(1)
+except:
+    print ("Externer Socket konnte nicht initialisiert werden!")
+    sys.exit(1)
 
-
-
-#socket_int_rec2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#socket_int_rec2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#socket_int_rec2.bind(INT)
-
-#except:
-    #print ("Could not set up EXT socket.")
-    #sys.exit(1)
 
 def int_to_ext():
-    global LOCALVPNPORT
+    global localvpnport
     global SERVER
-    global LOCALVPN
     global socket_int_rec
     global socket_ext_send
-    #Performanceupdate
-    #Has to be tested
-    socket_int_rec.settimeout(1)
     while True:
         try:
             data, addr = socket_int_rec.recvfrom(4096)
             if(not SERVER):
-                LOCALVPNPORT=addr[1]
+                localvpnport=addr[1]
             socket_ext_send.sendto(data, PEER)
             print("int to ext")
         except:
             pass
 
+
 def ext_to_int():
-    global LOCALVPNPORT
+    global localvpnport
     global SERVER
     global LOCALVPN
     global socket_int_send
     global socket_ext_rec
-    #Performanceupdate
-    #Has to be tested
-    socket_ext_rec.settimeout(1)
     while True:
         try:
             data, addr = socket_ext_rec.recvfrom(4096)
             if(not SERVER):
-                socket_int_send.sendto(data, ("localhost", LOCALVPNPORT))
+                socket_int_send.sendto(data, ("localhost", localvpnport))
             if(SERVER):
                 socket_int_send.sendto(data, LOCALVPN)
             print("ext to int")
         except:
             pass
-
 
 
 t_int_to_ext = threading.Thread(target=int_to_ext)
