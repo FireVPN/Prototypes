5ea8463e76410ee4a5bb10ebfd1d10f8eb0e4770
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-12-29 18:54:49 +0100

    Dritter Ansatz

--- Python+OpenVPN/Proxy/TCP.py
+++ Python+OpenVPN/Proxy/TCP.py
@@ -8,65 +8,55 @@ LOCALVPN=("localhost", 6220)
 INT=("localhost", 6221)
 
 class Init():
-    def __init__(self):
-        print("Starting Proxy")
-        self.ext_sock = socket.socket()
-        self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        if("Windows" not in platform.platform()):
-            self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.ext_sock.bind(EXT)
-        self.int_sock = socket.socket()
-        self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        if("Windows" not in platform.platform()):
-            self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.int_sock.bind(INT)
-        if(not SERVER):
-            self.int_sock.listen(5)
-            connint, addr = self.int_sock.accept()
-            localvpnport=addr[1]
-            print("Verbindung zu VPN hergestellt")
-            self.ext_sock.connect(PEER)
-            print("Verbindung zu PEER hergestellt")
-            proxy_intern = Proxy_intern(connint, self.ext_sock)
-            proxy_extern = Proxy_extern(connint, self.ext_sock)
-        else:
-            self.int_sock.connect(LOCALVPN)
-            print("Verbindung zu VPN hergestellt")
-            self.ext_sock.listen(5)
-            connext, addr = self.ext_sock.accept()
-            print("Verbindung zu PEER hergestellt")
-            proxy_intern = Proxy_intern(self.int_sock, connext)
-            proxy_extern = Proxy_extern(self.int_sock, connext)
-        tintern = threading.Thread(target=proxy_intern.run())
-        textern = threading.Thread(target=proxy_extern.run())
-        tintern.start()
-        textern.start()
+   def __init__(self):
+       print("Starting Proxy")
+       self.ext_sock = socket.socket()
+       self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+       if("Windows" not in platform.platform()):
+           self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+       self.ext_sock.bind(EXT)
+       self.int_sock = socket.socket()
+       self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+       if("Windows" not in platform.platform()):
+           self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+       self.int_sock.bind(INT)
+       if(not SERVER):
+           self.int_sock.listen(5)
+           connint, addr = self.int_sock.accept()
+           localvpnport=addr[1]
+           print("Verbindung zu VPN hergestellt")
+           self.ext_sock.connect(PEER)
+           print("Verbindung zu PEER hergestellt")
+           proxy_extern = Proxy(connint.dup(), self.ext_sock.dup())
+       else:
+           self.int_sock.connect(LOCALVPN)
+           print("Verbindung zu VPN hergestellt")
+           self.ext_sock.listen(5)
+           connext, addr = self.ext_sock.accept()
+           print("Verbindung zu PEER hergestellt")
+           proxy_intern = Proxy(self.int_sock.dup(), connext.dup())
+       tintern = threading.Thread(target=Proxy.intern())
+       textern = threading.Thread(target=Proxy.extern())
+       tintern.start()
+       textern.start()
 
-class Proxy_intern():
-    def __init__(self, int_sock, ext_sock):
-        self.int_sock = int_sock
-        self.ext_sock = ext_sock
-    def run(self):
-        while True:
-            try:
-                data = self.int_sock.recv(1024)
-                self.ext_sock.send(data)
-            except:
-                pass
+class Proxy():
+   def __init__(self, int_sock, ext_sock):
+       self.int_sock = int_sock
+       self.ext_sock = ext_sock
+   def intern(self):
+       while True:
+           data = self.int_sock.recv(4096)
+           self.ext_sock.send(data)
+   def extern(self):
+       while True:
+           data = self.ext_sock.recv(4096)
+           self.int_sock.send(data)
 
-class Proxy_extern():
-    def __init__(self, int_sock, ext_sock):
-        self.int_sock = int_sock
-        self.ext_sock = ext_sock
-    def run(self):
-        while True:
-            try:
-                data = self.ext_sock.recv(1024)
-                self.int_sock.send(data)
-            except:
-                pass
 
 def main():
-    init = Init()
+   init = Init()
 if __name__ == '__main__':
-    main()
+   main()
+
+
