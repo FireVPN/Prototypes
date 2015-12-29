85c51611fc03c27c382e4b7c1d1c53d397c5b16a
eliaseckenfellner <elias.eckenfellner@edu.htl.rennweg.at>
2015-12-29 18:09:11 +0100

    Zweiter Ansatz

--- Python+OpenVPN/Proxy/TCP.py
+++ Python+OpenVPN/Proxy/TCP.py
@@ -7,86 +7,54 @@ localvpnport=0
 LOCALVPN=("localhost", 6220)
 INT=("localhost", 6221)
 
-try:
-    socket_int_rec = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_int_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_int_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_int_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_int_rec.bind(INT)
-    socket_int_send.bind(INT)
+class Init():
+    def __init__(self):
+        print("Starting Proxy")
+        self.ext_sock = socket.socket()
+        self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.ext_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.ext_sock.bind(EXT)
+        self.int_sock = socket.socket()
+        self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.int_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.int_sock.bind(INT)
+        if(not SERVER):
+            self.int_sock.listen(5)
+            connint, addr = self.int_sock.accept()
+            localvpnport=addr[1]
+            self.ext_sock.connect(PEER)
+            proxy_intern = Proxy_intern(connint, self.ext_sock)
+            proxy_extern = Proxy_extern(connint, self.ext_sock)
+        else:
+            self.int_sock.connect(LOCALVPN)
+            self.ext_sock.listen(5)
+            connext, addr = self.ext_sock.accept()
+            proxy_intern = Proxy_intern(self.int_sock, connext)
+            proxy_extern = Proxy_extern(self.int_sock, connext)
+        tintern = threading.Thread(target=proxy_intern.run())
+        textern = threading.Thread(target=proxy_extern.run())
+        tintern.start()
+        textern.start()
 
-    #Performance
-    socket_int_rec.settimeout(1)
-except:
-    print ("Interner Socket konnte nicht initialisiert werden!")
-    sys.exit(1)
+class Proxy_intern():
+    def __init__(self, int_sock, ext_sock):
+        self.int_sock = int_sock
+        self.ext_sock = ext_sock
+    def run(self):
+        while True:
+            data = self.int_sock.recv(1024)
+            self.ext_sock.send(data)
 
-try:
-    socket_ext_rec = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_ext_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_ext_rec.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_ext_send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_ext_rec.bind(EXT)
-    socket_ext_send.bind(EXT)
+class Proxy_extern():
+    def __init__(self, int_sock, ext_sock):
+        self.int_sock = int_sock
+        self.ext_sock = ext_sock
+    def run(self):
+        while True:
+            data = self.ext_sock.recv(1024)
+            self.int_sock.send(data)
 
-    #Performance
-    socket_ext_rec.settimeout(1)
-except:
-    print ("Externer Socket konnte nicht initialisiert werden!")
-    sys.exit(1)
-
-
-def int_to_ext():
-    global localvpnport
-    global SERVER
-    global socket_int_rec
-    global socket_ext_send
-    while True:
-        try:
-            socket_int_rec.listen(1)
-            conn, addr = socket_int_rec.accept()
-            while True:
-                data = conn.recv(4096)
-                if not data:
-                    break
-            if(not SERVER):
-                localvpnport=addr[1]
-            socket_ext_send.connect(PEER)
-            socket_ext_send.send(data)
-            socket_ext_send.close()
-            print("int to ext")
-        except:
-            pass
-
-
-def ext_to_int():
-    global localvpnport
-    global SERVER
-    global LOCALVPN
-    global socket_int_send
-    global socket_ext_rec
-    while True:
-        try:
-            socket_ext_rec.listen(1)
-            conn, addr = socket_ext_rec.accept()
-            while True:
-                data = conn.recv(4096)
-                if not data:
-                    break
-            if(not SERVER):
-                socket_int_send.connect(("localhost", localvpnport))
-                socket_int_send.send(data)
-                socket_int_send.close()
-            if(SERVER):
-                socket_int_send.connect(LOCALVPN)
-                socket_int_send.send(data)
-                socket_int_send.close()
-            print("ext to int")
-        except:
-            pass
-
-
-t_int_to_ext = threading.Thread(target=int_to_ext)
-t_ext_to_int = threading.Thread(target=ext_to_int)
-t_int_to_ext.start()
-t_ext_to_int.start()
+def main():
+    init = Init()
+if __name__ == '__main__':
+    main()
